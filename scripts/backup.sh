76902a395a0acb878a5dd2bd2f832862891628f2
neocizee <manuelcodesal9@gmail.com>
2025-11-12 21:30:44 -0300

    v0.0.1

--- scripts/backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# scripts/backup.sh
-
-# Este script se encargará de hacer backups del mundo del servidor.
-# Ejemplo:
-# TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
-# cp -r ../server/production/data/world ./backups/world_$TIMESTAMP
-echo "Backup script placeholder"
